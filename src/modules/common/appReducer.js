00f6faa1aeee27fc083785921ed21704a1068a23
Scott Brabson <scottbrabson@gmail.com>
2021-02-20 16:48:30 -0700

    Add theme object for style access in state

--- src/modules/common/appReducer.js
+++ src/modules/common/appReducer.js
@@ -1,7 +1,10 @@
 
+import theme from '../../ui/common/theme'
+
 export const initialState = {
   missions: [{id:1, title: 'eat bugs', description: 'find and eat 3 bugs'}],
   user: { name: "scott" },
+  theme,
 };
 
 export const appReducer = (state, action) => {
--- src/modules/views/MissionControl.jsx
+++ src/modules/views/MissionControl.jsx
@@ -3,7 +3,8 @@ import AppContext from "../app/AppContext";
 
 const MissionControl = props => {
   const [state, dispatch] = useContext(AppContext)
-
+  const {colors, shadows, gradients, fonts} = state.theme
+  
   return (
     <div>
       <h1>I'm a Mission Control Component</h1>
