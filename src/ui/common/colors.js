f8c22166e5e99956eb7cd46531ebadd609d64830
Scott Brabson <scottbrabson@gmail.com>
2021-02-20 11:39:46 -0700

    Add tertiary color scheme

--- src/ui/common/colors.js
+++ src/ui/common/colors.js
@@ -1,12 +1,13 @@
 // Colors
 
 export const primary = '#7367F0'
-
 export const primaryAccent = '#CE9FFC'
 
 export const secondary = '#EA5455'
-
 export const secondaryAccent = '#FEB692'
+
+export const tertiary = '#EA5455'
+export const tertiaryAccent = '#FEB692'
 
 export const white = '#ffffff'
 
