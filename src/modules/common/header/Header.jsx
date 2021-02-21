bc7bbb91a264ff3a51ed9d0dae583ea52a5e751a
Scott Brabson <scottbrabson@gmail.com>
2021-02-20 20:36:18 -0700

    add header component

---
+++ src/modules/common/header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from "../../../ui/button/Button";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(primary => ({
+  navbar: {
+    background: "rebeccapurple",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "0 6%",
+  },
+  linkContainer: {
+    display: "flow-root",
+    flex: 1,
+    direction: "rtl",
+  },
+  left: {
+    flex: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <header className={classes.navbar}>
+      <section className={classes.left}>
+        <h1>BRANDING + NAVBAR</h1>
+      </section>
+      <nav className={classes.linkContainer}>
+        <Link className={classes} title="mission control" to="/mission-control">
+          <Button>Mission Control</Button>
+        </Link>
+        <Link className={classes} title="style guide" to="/style-guide">
+          <Button>Style Guide</Button>
+        </Link>
+        <Link className={classes} title="home" to="/">
+          <Button>Home</Button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
