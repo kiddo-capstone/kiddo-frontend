3d217729373b2bda43f844f16f90dce70743fe01
Scott Brabson <scottbrabson@gmail.com>
2021-02-20 21:11:22 -0700

    add styleguide colors

--- src/modules/common/header/Header.jsx
+++ src/modules/common/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../../../ui/button/Button";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
+import * as colors from '../../../ui/common/colors'
 
 const useStyles = makeStyles(primary => ({
   navbar: {
@@ -18,6 +19,8 @@ const useStyles = makeStyles(primary => ({
   },
   left: {
     flex: 1,
+    fontFamily: 'Russo One',
+    color: colors.green,
   },
 }));
 
