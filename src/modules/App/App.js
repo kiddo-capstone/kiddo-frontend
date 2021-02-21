f286e818b288dd814f5a0551bd424e0e9e557471
Scott Brabson <scottbrabson@gmail.com>
2021-02-20 20:36:09 -0700

    Rebase main into branch

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -2,50 +2,25 @@ import React, { useReducer, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import AppContext from "./AppContext";
 import { appReducer, initialState } from "../common/appReducer";
-import "./App.css";
 import MissionControl from "../views/MissionControl";
 import StyleSample from "../views/StyleSample";
-import Button from '../../ui/button/Button';
 import DailyMission from "../views/DailyMission";
 import Sidebar from "../common/sidebar/Sidebar";
+import Header from "../common/header/Header";
 
 function App() {
   const [state, dispatch] = useReducer(appReducer, initialState);
-  const [show, setShow] = useState(true)
 
   return (
     <AppContext.Provider value={[state, dispatch]}>
-      <main className="App">
-        <Sidebar />
-        <div className='routes'>
-          <Switch>
-            <Route
-              exact
-              path='/agent-dashboard'
-              render={() =>
-                <MissionControl />
-              }
-            />
-            <Route
-              exact
-              path='/daily-mission'
-              render={() =>
-                <DailyMission />
-              }
-            />
-            <Route
-              exact
-              path='/style-guide'
-              render={() =>
-                <>
-                  <Button onClick={() => setShow(!show)}>{show ? "Hide Style Guide" : "Show Style Guide"}</Button>
-                  {show && <StyleSample />}
-                </>
-              }
-            />
-          </Switch>
-        </div>
-      </main>
+      <Header />
+      <Sidebar />
+      <Switch>
+        <Route path="/agent-dashboard" component={MissionControl}/>
+        <Route path="/daily-mission" component={DailyMission} />
+        <Route path="/style-guide" component={StyleSample} />
+        <Route path="/mission-control" component={MissionControl} />
+      </Switch>
     </AppContext.Provider>
   );
 }
