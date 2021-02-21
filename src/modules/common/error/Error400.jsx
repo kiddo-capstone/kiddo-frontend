fd055cd878a7b0e613efb3a74966f01f175216f8
Bailey Dunning <bwdunning@gmail.com>
2021-02-21 13:40:24 -0700

    Add btn to 400 error to go back to mission control

--- src/modules/common/error/Error400.jsx
+++ src/modules/common/error/Error400.jsx
@@ -1,6 +1,16 @@
+import React from "react";
+import Button from "../../../ui/button/Button";
+import { useHistory } from "react-router-dom";
+import theme from "../../../ui/common/theme";
+
 const Error400 = () => {
+  const history = useHistory()
   return (
-    <h3>Looks like you got lost!</h3>
+    <section style={{color: `${theme.colors.white}`, textAlign: "center"}}>
+      <h1>Looks like you got lost!</h1>
+      <h3>Unfortunately, this page doesn't exist (yet)</h3>
+      <Button onClick={() => history.push("/mission-control")}>Take me back to mission control!</Button>
+    </section>
   )
 }
 
