a98a82c621f895441350e2b0afb989a2d51283b1
Bailey Dunning <bwdunning@gmail.com>
2021-02-21 12:37:30 -0700

    Convert ErrorBoundary to class component

--- src/modules/common/error/ErrorBoundary.jsx
+++ src/modules/common/error/ErrorBoundary.jsx
@@ -1,7 +1,13 @@
-const ErrorBoundary = () => {
-  return (
-    <h3>All systems down!</h3>
-  )
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    <h3>All systems down</h3>
+  }
 }
 
 export default ErrorBoundary
