1607df6a46210f129fa896f6d7682d51b2566055
Scott Brabson <scottbrabson@gmail.com>
2021-02-22 19:56:41 -0700

    add missions to MC

--- src/modules/views/MissionControl.jsx
+++ src/modules/views/MissionControl.jsx
@@ -1,22 +1,39 @@
 import React, { useContext } from "react";
 import AppContext from "../app/AppContext";
-import PageContainer from "../../ui/containers/PageContainer";
-import TitleContainer from "../../ui/containers/TitleContainer";
+import {PageContainer, TitleContainer, SmallContainer, MediumContainer} from "../../ui/containers/index";
 import Button from "../../ui/button/Button";
+import Mission from '../mission/Mission'
 import { Link } from "react-router-dom";
+import {makeStyles} from '@material-ui/core'
+
+const useStyles = makeStyles(primary => ({
+  missions: {
+    justifyContent: 'space-between',
+    height: '90%'
+  }
+}))
 
 const MissionControl = props => {
   const [state, dispatch] = useContext(AppContext);
   const { colors, shadows, gradients, fonts } = state.theme;
+  
+  const classes = useStyles()
+
+  const mapMissions = () => {
+    return state.missions.map(mission => {
+      return <Mission
+        key={mission.id}
+        props={mission}
+      />
+    })
+  }
 
   return (
     <PageContainer>
-      <TitleContainer>
-        <h1>I'm a Mission Control Component</h1>
-        <Link title="Mission" to="daily-mission">
-          <Button>Go to mission</Button>
-        </Link>
-      </TitleContainer>
+      <TitleContainer>Mission Control</TitleContainer>
+      <MediumContainer className={classes.missions}>
+        {mapMissions()}
+      </MediumContainer>
     </PageContainer>
   );
 };
