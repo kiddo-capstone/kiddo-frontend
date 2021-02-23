600f3dfdee19bcf4d3787b9366f8382b25464af3
Bailey Dunning <bwdunning@gmail.com>
2021-02-23 12:11:49 -0700

    Add error to state and conditionally render

--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -7,29 +7,30 @@ import StyleSample from "../views/StyleSample";
 import DailyMission from "../views/DailyMission";
 import Error400 from "../common/error/Error400";
 import Error500 from "../common/error/Error500";
-import { ErrorBoundary } from "react-error-boundary";
 import AppContainer from "../../ui/containers/AppContainer";
 import { getAllMissions, getAllTasks, getAllUsers } from "../common/apiCalls";
 
-function App() {
+const App = () => {
   const [state, dispatch] = useReducer(appReducer, initialState);
+  const [error, setError] = useState(null);
+  // const handleError = useErrorHandler();
 
-  useEffect(async() => {
+  useEffect(async () => {
     await getAllMissions()
       .then(data => addDataToState('missions', data.data))
-      .catch(error => console.log(error))
+      .catch(error => setError(error))
   }, [])
 
-  useEffect(async() => {
+  useEffect(async () => {
     await getAllTasks()
       .then(data => addDataToState('tasks', data.data))
-      .catch(error => console.log(error))
+      .catch(error => setError(error))
   }, [])
 
-  useEffect(async() => {
+  useEffect(async () => {
     await getAllUsers()
       .then(data => addDataToState('users', data.data))
-      .catch(error => console.log(error))
+      .catch(error => setError(error))
   }, [])
 
   const addDataToState = (type, data) => {
@@ -38,26 +39,27 @@ function App() {
   }
 
   return (
-    <ErrorBoundary FallbackComponent={Error500}>
-      <AppContext.Provider value={[state, dispatch]}>
-        <AppContainer />
-        <Switch>
-          <Route
-            exact
-            path="/daily-mission/:id"
-            render={({ match }) => {
-              const id = +match.params.id;
-              return <DailyMission id={id} />;
-            }}
-          />
-          <Route exact path="/style-guide" component={StyleSample} />
-          <Route exact path="/mission-control" component={MissionControl} />
-          <Route exact path="/error-500" component={Error500} />
-          <Route path="/" component={Error400} />
-        </Switch>
-      </AppContext.Provider>
-    </ErrorBoundary>
+    <>
+      { !error ?
+        <AppContext.Provider value={[state, dispatch]}>
+          <AppContainer />
+          <Switch>
+            <Route
+              exact
+              path="/daily-mission/:id"
+              render={({ match }) => {
+                const id = +match.params.id;
+                return <DailyMission id={id} />;
+              }}
+            />
+            <Route exact path="/style-guide" component={StyleSample} />
+            <Route exact path="/mission-control" component={MissionControl} />
+            <Route path="/" component={Error400} />
+          </Switch>
+        </AppContext.Provider> :
+        <Error500 /> }
+    </>
   );
 }
 
-export default App;
+export default React.memo(App);
