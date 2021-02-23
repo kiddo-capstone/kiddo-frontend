f3495606e25bdfb79554fa3b0dee03b5c81a29d5
Bailey Dunning <bwdunning@gmail.com>
2021-02-23 14:28:52 -0700

    Add deployed backend to api calls

--- src/modules/common/apiCalls.js
+++ src/modules/common/apiCalls.js
@@ -1,20 +1,20 @@
 export const getAllMissions = () => {
   return (
-    fetch('http://localhost:3000/api/v1/missions')
+    fetch('https://kiddo-backend.herokuapp.com//api/v1/missions')
     .then(res => res.json())
   )
 }
 
 export const getAllTasks = () => {
   return (
-    fetch('http://localhost:3000/api/v1/tasks')
+    fetch('https://kiddo-backend.herokuapp.com/api/v1/tasks')
       .then(res => res.json())
   )
 }
 
 export const getAllUsers = () => {
   return (
-    fetch('http://localhost:3000/api/v1/users')
+    fetch('https://kiddo-backend.herokuapp.com//api/v1/users')
       .then(res => res.json())
   )
 }
