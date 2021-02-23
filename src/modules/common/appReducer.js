6bd07a73b1748373323477468638c7f81c3f1b62
Bailey Dunning <bwdunning@gmail.com>
2021-02-22 18:41:36 -0700

    Add case for FETCH_TASKS

--- src/modules/common/appReducer.js
+++ src/modules/common/appReducer.js
@@ -12,6 +12,8 @@ export const appReducer = (state, action) => {
   switch(action.type) {
     case 'FETCH_MISSIONS':
       return {...state, missions: action.missions}
+    case 'FETCH_TASKS':
+      return { ...state, tasks: action.tasks }
     default:
       return state;
   }
