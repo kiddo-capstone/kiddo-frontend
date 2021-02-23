f6beee26e2c63fe2328b3efad3c1f501666094a0
Bailey Dunning <bwdunning@gmail.com>
2021-02-22 18:38:18 -0700

    Add method to api calls to GET tasks

--- src/modules/common/apiCalls.js
+++ src/modules/common/apiCalls.js
@@ -4,3 +4,10 @@ export const getAllMissions = () => {
     .then(res => res.json())
   )
 }
+
+export const getAllTasks = () => {
+  return (
+    fetch('http://localhost:3000/api/v1/tasks')
+      .then(res => res.json())
+  )
+}
