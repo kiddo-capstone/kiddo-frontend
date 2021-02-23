c6ecbd9be80083289c5bd9e98a621c30e4d479d7
Bailey Dunning <bwdunning@gmail.com>
2021-02-22 18:37:24 -0700

    Add case for FETCH_MISSIONS

--- src/modules/common/appReducer.js
+++ src/modules/common/appReducer.js
@@ -2,16 +2,16 @@
 import theme from '../../ui/common/theme'
 
 export const initialState = {
-  missions: [{id:1, title: 'eat bugs', description: 'find and eat 3 bugs'}],
-  user: { name: "scott" },
+  users: [],
+  missions: [],
+  tasks: [],
   theme,
 };
 
 export const appReducer = (state, action) => {
   switch(action.type) {
-    case 'SOME_CASE':
-      // DO THINGS
-      return {...state}
+    case 'FETCH_MISSIONS':
+      return {...state, missions: action.missions}
     default:
       return state;
   }
