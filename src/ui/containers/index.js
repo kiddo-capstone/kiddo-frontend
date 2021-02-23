493176de7931f564a7e80c4d769763d2208e9d63
Scott Brabson <scottbrabson@gmail.com>
2021-02-22 19:48:41 -0700

    create container index file

---
+++ src/ui/containers/index.js
@@ -0,0 +1,6 @@
+import PageContainer from './PageContainer'
+import TitleContainer from './TitleContainer'
+import SmallContainer from './SmallContainer'
+import MediumContainer from './MediumContainer'
+
+export {TitleContainer, PageContainer, SmallContainer, MediumContainer}
