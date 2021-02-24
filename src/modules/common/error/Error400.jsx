1b7911c5a3157e177e3874ea14b737efefe298ce
Bailey Dunning <bwdunning@gmail.com>
2021-02-24 15:25:23 -0700

    add page container to error components

--- src/modules/common/error/Error400.jsx
+++ src/modules/common/error/Error400.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Button from "../../../ui/button/Button";
 import { useHistory } from "react-router-dom";
 import theme from "../../../ui/common/theme";
+import { PageContainer } from "../../../ui/containers";
 
 const Error400 = () => {
   const history = useHistory()
   return (
-    <section style={{color: `${theme.colors.white}`, textAlign: "center"}}>
-      <h1>Looks like you got lost!</h1>
-      <h3>Unfortunately, this page doesn't exist (yet)</h3>
-      <Button onClick={() => history.push("/mission-control")}>Take me back to mission control!</Button>
-    </section>
+    <PageContainer>
+      <section style={{color: `${theme.colors.white}`, textAlign: "center"}}>
+        <h1>Looks like you got lost!</h1>
+        <h3>Unfortunately, this page doesn't exist (yet)</h3>
+        <Button onClick={() => history.push("/mission-control")}>Take me back to mission control!</Button>
+      </section>
+    </PageContainer >
   )
 }
 
