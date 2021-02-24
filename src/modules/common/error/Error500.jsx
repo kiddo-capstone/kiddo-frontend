57a61e4864754a6d94b33f15aa6d484adbd712ad
Bailey Dunning <bwdunning@gmail.com>
2021-02-24 15:25:23 -0700

    add page container to error components

--- src/modules/common/error/Error500.jsx
+++ src/modules/common/error/Error500.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import theme from "../../../ui/common/theme";
+import { PageContainer } from "../../../ui/containers";
 
 const Error500 = () => {
   return (
-    <section style={{ color: `${theme.colors.white}`, textAlign: "center" }}>
-      <h1> Uh oh! Our systems are down. 😖 </h1>
-      <h2> Mission Control will be up and running again as soon as possible. </h2>
-    </section>
+    <PageContainer>
+      <section style={{ color: `${theme.colors.white}`, textAlign: "center" }}>
+        <h1> Uh oh! Our systems are down. 😖 </h1>
+        <h2> Mission Control will be up and running again as soon as possible. </h2>
+      </section>
+    </PageContainer>
   )
 }
 
