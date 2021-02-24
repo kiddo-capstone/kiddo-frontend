395998313fe55c0b4e62b0ea3064b2146942397d
Scott Brabson <scottbrabson@gmail.com>
2021-02-24 16:02:08 -0700

    update state with selewcted mission

--- src/modules/common/appReducer.js
+++ src/modules/common/appReducer.js
@@ -13,6 +13,8 @@ export const appReducer = (state, action) => {
   switch(action.type) {
     case 'FETCH_MISSIONS':
       return {...state, missions: action.missions}
+    case 'FETCH_SELECTED_MISSION':
+      return {...state, selectedMission: action.selectedMission}
     case 'FETCH_TASKS':
       return { ...state, tasks: action.tasks }
     case 'FETCH_USERS':
