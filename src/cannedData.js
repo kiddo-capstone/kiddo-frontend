c5d97c04fcde58bfd85f02eaa8312452c2f20f68
Scott Brabson <scottbrabson@gmail.com>
2021-02-24 16:48:34 -0700

    add canned data for development

---
+++ src/cannedData.js
@@ -0,0 +1,44 @@
+export const cannedData = {
+  "data": [
+      {
+          "id": "1",
+          "type": "task",
+          "attributes": {
+              "name": "EQ level up",
+              "description": "Say something kind",
+              "category": "EQ",
+              "points": 100
+          }
+      },
+      {
+          "id": "2",
+          "type": "task",
+          "attributes": {
+              "name": "IQ level up",
+              "description": "Conquer homework",
+              "category": "IQ",
+              "points": 50
+          }
+      },
+      {
+          "id": "3",
+          "type": "task",
+          "attributes": {
+              "name": "Special",
+              "description": "Make your bed",
+              "category": "Misc",
+              "points": 1337
+          }
+      },
+      {
+          "id": "4",
+          "type": "task",
+          "attributes": {
+              "name": "test",
+              "description": "testing",
+              "category": "test",
+              "points": 3
+          }
+      }
+  ]
+}
