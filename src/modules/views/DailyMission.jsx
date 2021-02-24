5e660c3d4e429e4acbd705ff48a22646e65baf13
Scott Brabson <scottbrabson@gmail.com>
2021-02-24 16:01:59 -0700

    implement API call

--- src/modules/views/DailyMission.jsx
+++ src/modules/views/DailyMission.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from "react";
+import AppContext from "../app/AppContext";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import { getMissionById } from "../common/apiCalls";
+
 import PageContainer from "../../ui/containers/PageContainer";
 import TitleContainer from "../../ui/containers/TitleContainer";
 
-const DailyMission = () => {
+const useStyles = makeStyles(theme => ({}));
+
+const DailyMission = props => {
+  const [state, dispatch] = useContext(AppContext);
+  const [error, setError] = useState(null);
+  const { id } = props;
+
+  useEffect(async () => {
+  await getMissionById(id)
+    .then(data => addMissionToState('selectedMission', data.data))
+    .catch(error => setError(error))
+  }, [])
+
+  const addMissionToState = (type, data) => {
+    const action = { type: `FETCH_SELECTED_MISSION`, selectedMission: data }
+    dispatch(action)
+  }
+
+
   return (
     <PageContainer>
       <TitleContainer>
-        <h1>Daily Mission</h1>
+        <h1>{id}</h1>
       </TitleContainer>
     </PageContainer>
   );
