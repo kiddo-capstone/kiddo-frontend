fb532bb13021c4c1f1824ae7cd3867c485a7a576
Scott Brabson <scottbrabson@gmail.com>
2021-02-25 15:07:09 -0700

    add logic for jounral component

--- src/modules/tasks/Journal.js
+++ src/modules/tasks/Journal.js
@@ -1,29 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   textBox: {
-    fontSize: '1em',
-    width: '90%',
-    height: '100%',
-    height: '100%',
-    border: 'solid 2px hotpink',
-    padding: '1em',
-    borderRadius: '10px',
-    backgroundColor: 'rgb(40,44,52, .5)',
-    outline: 'none',
-    color: 'white',
-  }
+    fontSize: "1em",
+    width: "100%",
+    height: "100%",
+    border: "solid 3px hotpink",
+    padding: "1em",
+    borderRadius: "10px",
+    backgroundColor: "rgb(40,44,52, .5)",
+    outline: "none",
+    color: "white",
+  },
 }));
 
-const Journal = props => {
-  const [entry, setEntry] = useState('')
+const Journal = ({checkReady}) => {
+  const [entry, setEntry] = useState("");
   const classes = useStyles();
+
+  const handleChange = (value) => {
+    setEntry(value)
+    entry ? checkReady(true) : checkReady(false)
+  }
 
   return (
     <section>
       <form>
-        <textarea rows="10" cols="60" className={classes.textBox} type="text" value={entry} name="entry" placeholder="Write you answer here and click submit when you're done!" onChange={event => setEntry(event.target.value)}></textarea>
+        <textarea
+          rows="10"
+          cols="60"
+          className={classes.textBox}
+          type="text"
+          value={entry}
+          name="entry"
+          placeholder="Write your answer here and click submit when you're done!"
+          onChange={event => handleChange(event.target.value)}></textarea>
       </form>
       {/* <input type='submit'/> */}
     </section>
