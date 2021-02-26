7170c4c2435a62652e0d64303619e6c683997a16
Bailey Dunning <bwdunning@gmail.com>
2021-02-25 20:34:31 -0700

    add image to error500 component

--- src/modules/common/error/Error500.jsx
+++ src/modules/common/error/Error500.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import theme from "../../../ui/common/theme";
 import { PageContainer } from "../../../ui/containers";
+import serverDown from "../../../assets/servers-down.png";
 
 const Error500 = () => {
   return (
-    <PageContainer>
       <section style={{ color: `${theme.colors.white}`, textAlign: "center" }}>
         <h1> Uh oh! Our systems are down. 😖 </h1>
         <h2> Mission Control will be up and running again as soon as possible. </h2>
+        <img src={serverDown} style={{width: '60%'}} alt="all servers down"/>
       </section>
-    </PageContainer>
   )
 }
 
