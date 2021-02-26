cafdfcfd81bd0b38dc8ef104fb61ca0370a2ccc9
Bailey Dunning <bwdunning@gmail.com>
2021-02-25 20:22:19 -0700

    simplify auth component

--- src/modules/auth/Auth.js
+++ src/modules/auth/Auth.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { PageContainer, TitleContainer } from "../../ui/containers/index";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "../../ui/button/Button";
 
@@ -18,9 +17,8 @@ const LogoutButton = () => {
   );
 };
 
-const Profile = () => {
+const Auth = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  console.log(user);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
@@ -28,22 +26,6 @@ const Profile = () => {
   return (
     <>
       {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
-      {isAuthenticated && (
-        <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
-      )}
-    </>
-  );
-};
-
-const Auth = ({isAuthenticated}) => {
-  return (
-    <>
-      <TitleContainer>{!isAuthenticated ? 'Click below to log into your account' : 'Click below to log out of your account'}</TitleContainer>
-      <Profile />
     </>
   );
 };
