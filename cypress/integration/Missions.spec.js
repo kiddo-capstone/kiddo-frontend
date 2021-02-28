49547e58eeee57ffcc2cc69258e2e9c9715cc94b
Bailey Dunning <bwdunning@gmail.com>
2021-02-28 16:04:40 -0700

    test ux for going through mission

---
+++ cypress/integration/Missions.spec.js
@@ -0,0 +1,32 @@
+describe("MissionControl", () => {
+  beforeEach(() => {
+    cy.visit("/mission-control")
+  });
+
+  it("should display information on a mission card", () => {
+    cy.contains("Independence Day")
+  })
+
+  it("should allow a user to start a mission and submit a task", () => {
+    cy.get(':nth-child(1) > .makeStyles-mission-55 > .makeStyles-right-58 > a > .makeStyles-round-63')
+      .click()
+    cy.url().should("include", "/daily-mission/")
+    cy.contains("Agent Task")
+      .click()
+    
+    cy.url().should("include", "/task/")
+
+    cy.get(".makeStyles-textBox-91")
+      .type("Hello! ")
+
+    cy.get('.makeStyles-pageContainer-86 > .makeStyles-butt-41')
+      .should('be.disabled')
+
+    cy.get(".makeStyles-textBox-91")
+      .type("So now I'm going to type at least fiften words because that's what the user has to do")
+    
+    cy.get('.makeStyles-pageContainer-86 > .makeStyles-butt-41')
+      .should('not.be.disabled')
+      .click()
+  });
+});
