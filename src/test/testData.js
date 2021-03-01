731e7551d09df221f42041724b8e414478cb6749
Bailey Dunning <bwdunning@gmail.com>
2021-03-01 11:57:44 -0700

    add test data

---
+++ src/test/testData.js
@@ -0,0 +1,47 @@
+export const missionsData = [
+  {
+    "id": 304,
+    "type": "mission",
+    "attributes": {
+      "name": "Weekly chores",
+      "due_date": "2001-02-03",
+      "user_id": 487,
+      "created_at": "2021-02-21T04: 05: 24.749Z",
+      "updated_at": "2021-02-21T04: 05: 24.749Z"
+    }
+  }
+]
+
+export const tasksData = [
+  {
+    "id": 1,
+    "type": "task",
+    "attributes": {
+      "name": "test1",
+      "description": "testing",
+      "category": "test",
+      "points": 3
+    }
+  },
+  {
+    "id": 2,
+    "type": "task",
+    "attributes": {
+      "name": "test2",
+      "description": "testing",
+      "category": "test",
+      "points": 3
+    }
+  }
+]
+
+export const usersData = [
+  {
+    "id": "1",
+    "type": "user",
+    "attributes": {
+      "name": "Calvin",
+      "email": "Calvin@example.com"
+    }
+  }
+]
