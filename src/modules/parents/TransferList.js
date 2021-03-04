e6eabd6b6a1abf74070e9c23ae30f7169054cd66
Scott Brabson <scottbrabson@gmail.com>
2021-03-03 21:50:19 -0700

    add xfer list w/logic for bubbling to parentview

---
+++ src/modules/parents/TransferList.js
@@ -0,0 +1,205 @@
+import React, { useState, useEffect, useContext } from "react";
+import AppContext from '../../modules/App/AppContext'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: "auto",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  cardHeader: {
+    padding: theme.spacing(1, 2),
+  },
+  list: {
+    width: 200,
+    height: 230,
+    backgroundColor: theme.palette.background.paper,
+    overflow: "auto",
+  },
+  button: {
+    margin: theme.spacing(0.5, 0),
+  },
+}));
+
+function not(a, b) {
+  return a.filter(value => b.indexOf(value) === -1);
+}
+
+function intersection(a, b) {
+  return a.filter(value => b.indexOf(value) !== -1);
+}
+
+function union(a, b) {
+  return [...a, ...not(b, a)];
+}
+
+export default function TransferList({getChoices}) {
+  const classes = useStyles();
+  const [state, dispatch] = useContext(AppContext);
+  const [checked, setChecked] = React.useState([]);
+  const [left, setLeft] = React.useState([]);
+  const [right, setRight] = React.useState([]);
+
+  useEffect(() => {
+    setLeft(generateTaskChoices())
+  }, [state.tasks])
+  
+  useEffect(() => {
+    const taskIdList = right.reduce((acc, t) => {
+      acc.push(t.charAt(0))
+      return acc
+    }, [])
+    
+    const taskObjects = taskIdList.reduce((acc, id) => {
+      const match = state.tasks.find(t => t.id === id)
+      acc.push(match)
+      return acc
+    }, [])
+
+    getChoices(taskObjects);
+  }, [right])
+  
+  const generateTaskChoices = () => {
+    return state.tasks.reduce((acc, t) => {
+      acc.push(t.id + ' ' + t.attributes.name)
+      return acc
+    }, [])
+  }
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const numberOfChecked = items => intersection(checked, items).length;
+
+  const handleToggleAll = items => () => {
+    if (numberOfChecked(items) === items.length) {
+      setChecked(not(checked, items));
+    } else {
+      setChecked(union(checked, items));
+    }
+  };
+
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
+  };
+
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const customList = (title, items) => (
+    <Card>
+      <CardHeader
+        className={classes.cardHeader}
+        avatar={
+          <Checkbox
+            onClick={handleToggleAll(items)}
+            checked={
+              numberOfChecked(items) === items.length && items.length !== 0
+            }
+            indeterminate={
+              numberOfChecked(items) !== items.length &&
+              numberOfChecked(items) !== 0
+            }
+            disabled={items.length === 0}
+            inputProps={{ "aria-label": "all items selected" }}
+          />
+        }
+        title={title}
+        subheader={`${numberOfChecked(items)}/${items.length} selected`}
+      />
+      <Divider />
+      <List className={classes.list} dense component="div" role="list">
+        {items.map(value => {
+          const labelId = `transfer-list-all-item-${value}-label`;
+
+          return (
+            <ListItem
+              key={value}
+              role="listitem"
+              button
+              onClick={handleToggle(value)}>
+              <ListItemIcon>
+                <Checkbox
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ "aria-labelledby": labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`${value}`} />
+            </ListItem>
+          );
+        })}
+        <ListItem />
+      </List>
+    </Card>
+  );
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justify="center"
+      alignItems="center"
+      className={classes.root}>
+      <Grid item>{customList("Tasks", left)}</Grid>
+      <Grid item>
+        <Grid container direction="column" alignItems="center">
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right">
+            &gt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left">
+            &lt;
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item>{customList("Assigned Tasks", right)}</Grid>
+    </Grid>
+  );
+}
