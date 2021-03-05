29ce6bc45367f620fcf67cfcc0f66a107d0461c8
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-03-05 00:07:05 -0700

    Change button text color for accessibility

--- src/ui/modal/ModalWrapper.js
+++ src/ui/modal/ModalWrapper.js
@@ -22,7 +22,6 @@ const useStyles = makeStyles(() => ({
     padding: ".2em",
     color: "white",
     minWidth: "100%",
-    // maxWidth: "100%",
     fontFamily: appStyles.fonts.primary,
     fontSize: "20px",
   },
@@ -33,37 +32,16 @@ const useStyles = makeStyles(() => ({
     fontFamily: appStyles.fonts.secondary,
   },
   button: {
-    fontSize: "1em",
-    fontWeight: "400",
-    padding: "2%",
     fontFamily: fonts.secondary,
-    borderRadius: "15px",
-    // previous theme:
-    // borderColor: ((theme) => theme.primary ? colors.darkBlue :  colors.darkPink),
-    // color: ((theme) => theme.primary ? colors.primaryAccent :  colors.secondaryAccent),
-    // background: 'transparent',
-    // background: ((theme) => theme.primary ? colors.blue :  colors.pink),
-    border: "solid 3px",
-    width: "40%",
-    borderColor: (theme) => (theme.primary ? colors.blue : colors.darkPink),
     background: "transparent",
-    color: (theme) => (theme.primary ? colors.blue : colors.darkPink),
     outline: "none",
     willChange: "transform",
     margin: "2%",
     cursor: "pointer",
-    // transition:
-    //   "transform ease .3s, border ease 2s, background ease .3s, color ease .3s",
+    transition:
+      "transform ease .3s, border ease 2s, background ease .3s, color ease .3s",
     "&:hover": {
-      // borderColor: ((theme) => theme.primary ? colors.darkBlue :  colors.darkPink),
-      background: (theme) => (theme.primary ? colors.blue : colors.darkPink),
-      color: (theme) => (theme.primary ? "#C0C0C1" : "#3E4452"),
-      transform: "translateY(-3%)",
-    },
-    "&:hover": {
-      borderColor: colors.grey1,
-      color: colors.grey3,
-      background: "none",
+      transform: "translateY(-5%)",
     },
   },
 }));
