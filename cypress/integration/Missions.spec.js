a2fe2646c71b2f751dd4261549cac5580696bea1
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-03-12 12:24:26 -0700

    Fix Mission test; Now passes

--- cypress/integration/Missions.spec.js
+++ cypress/integration/Missions.spec.js
@@ -1,23 +1,19 @@
 describe("MissionControl", () => {
   beforeEach(() => {
-    cy.visit("/mission-control")
+    cy.visit("/mission-control");
   });
 
   it("should display information on a mission card", () => {
-    cy.contains("Independence Day")
-      .should("be.visible")
-  })
+    cy.contains("Mission Possible").should("be.visible");
+  });
 
   it("should allow a user to start a mission and submit a task", () => {
-    cy.contains("START")
-      .should("be.visible")
-      .click()
+    cy.contains("START").should("be.visible").click();
 
-    cy.url().should("include", "/daily-mission/")
-    cy.contains("Agent Task")
-      .click()
-    
-    cy.url().should("include", "/task/")
+    cy.url().should("include", "/daily-mission/");
+    cy.contains("Agent Task").click();
+
+    cy.url().should("include", "/task/");
 
     // cy.get(".makeStyles-textBox-91")
     //   .type("Hello! ")
@@ -27,7 +23,7 @@ describe("MissionControl", () => {
 
     // cy.get(".makeStyles-textBox-91")
     //   .type("So now I'm going to type at least fiften words because that's what the user has to do")
-    
+
     // cy.get('a > .makeStyles-butt-41')
     //   .should("not.be.disabled")
     //   .click()
