76c0e974eb6d7933d84c0a93cfdb4c63c3b592fd
Scott Brabson <scottbrabson@gmail.com>
2021-03-18 11:02:43 -0600

    Show image and link to correct child MC

--- src/ui/card/Card.js
+++ src/ui/card/Card.js
@@ -1,4 +1,5 @@
 import React, {useContext, useState, useEffect} from 'react';
+import {Link} from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import AppContext from '../../modules/App/AppContext'
 import Card from '@material-ui/core/Card';
@@ -8,61 +9,59 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import kids_trio from '../../assets/kids_trio.png'
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: '30%',
+    maxWidth: '100%',
     width: '12em',
     minWidth: 200,
   },
 });
 
 
-function KiddoCard({child: {id, type, attributes: {name, parent_id, points}}}) {
+function KiddoCard({child}) {
   const [state, dispatch] = useContext(AppContext);
   const [missions, setMissions] = useState([])
-  const [avatar, setAvatar] = useState(null)
   const classes = useStyles();
+
+  const {id, type, attributes: {name, parent_id, points}} = child
 
   useEffect(() => {
     const userMissions = state.missions.filter(m => m.attributes.user_id === +id)
     setMissions(userMissions)
-    // console.log(missions)
-    // getAvatar() 
   }, [])
     
-  const getAvatar = async () => {
-    const parsed = name.replace(' ', '+')
-    const promAvatar = await fetch(`https://ui-avatars.com/api/?name=${parsed}`)
-      // .then((res) => res.json());
-    console.log(promAvatar);
-  };
+  const handleClick = () => {
+    const action = { type: `SET_CURRENT_USER`, currentUser: child }
+    dispatch(action)
+  }
 
   // return userMissions.map(m => (<Mission key={m.id} props={m} />))
 
   return (
-    <Card style={{margin: '.6em', filter: 'drop-shadow(2px 4px 6px black)'}} className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="a smiling profile photo"
-          height="200"
-          image={`https://ui-avatars.com/api/?background=random&name=${name.replace(' ', '+')}`}
-          title="KidDo User"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Agent Name: {name}
-          </Typography>
-          <Typography style={{display: 'flex', flexDirection: 'column'}} variant="body2" color="textSecondary" component="p">
-            <span>Total Points: {points}</span>
-            <span>Missions: {missions.length}</span>
-            <span>Completed Tasks: {missions.length}</span>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link style={{display: 'flex'}} to='/mission-control' onClick={() => handleClick()}>
+      <Card style={{margin: '.6em', filter: 'drop-shadow(2px 4px 6px black)'}} className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="a smiling profile photo"
+            height="200"
+            image={`https://ui-avatars.com/api/?background=random&name=${name.replace(' ', '+')}`}
+            title="KidDo User"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Agent Name: {name}
+            </Typography>
+            <Typography style={{display: 'flex', flexDirection: 'column'}} variant="body2" color="textSecondary" component="p">
+              <span>Total Points: {points}</span>
+              <span>Missions: {missions.length}</span>
+              <span>Completed Tasks: {missions.length}</span>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
 
