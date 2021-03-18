756bed68f121bf96edbf4976357629fc2f252c7c
Scott Brabson <scottbrabson@gmail.com>
2021-03-17 18:07:30 -0600

    fix bug duplicate users, create parent if dne

--- src/modules/common/apiCalls.js
+++ src/modules/common/apiCalls.js
@@ -103,3 +103,15 @@ export const createNewUser = (data) => {
     .then((response) => response.json())
     .catch((error) => console.log(error));
 };
+
+export const createNewParent = (data) => {
+  return fetch(`https://kiddo-backend.herokuapp.com/api/v1/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
