aae91bc6904c0c0006ebed3de56c1b2cb2c86cd8
Bailey Dunning <bwdunning@gmail.com>
2021-03-19 13:26:28 -0600

    add POST for new task

--- src/modules/parents/parentApiCalls.js
+++ src/modules/parents/parentApiCalls.js
@@ -12,6 +12,20 @@ export const createNewMission = (data) => {
   )
 };
 
+export const createNewTask = (data) => {
+  return (
+    fetch(`https://kiddo-backend.herokuapp.com/api/v1/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.log(error))
+  )
+}
+
 export const addTasksToMission = (data) => {
   return (
     fetch(`https://kiddo-backend.herokuapp.com/api/v1/mission_tasks`, {
