208e9ff9b788159cca5169d66e7536984cbc1f4c
Bailey Dunning <bwdunning@gmail.com>
2021-03-19 14:44:31 -0600

    update boolean logic for photo

--- src/modules/parents/TaskCreation.js
+++ src/modules/parents/TaskCreation.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import Button from "../../ui/button/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import ModalWrapper from "../../ui/modal/ModalWrapper";
+import Button from "../../ui/button/Button";
 
 const useStyles = makeStyles(() => ({
   modalContent: {
@@ -46,10 +46,17 @@ const TaskCreation = () => {
   }
   
   const handleTaskChange = (e) => {
-    console.log(task[e.target.name])
-    const updatedTask = { ...task, [e.target.name]: e.target.value }
+    const updatedValue = () => {
+      if (e.target.name === "photo") {
+        return e.target.value === "true" ? true : false
+      } else {
+        return e.target.value
+      }
+    }
     
-    setTask(updatedTask)
+    const updatedTask = { ...task, [e.target.name]: updatedValue() };
+    
+    setTask(updatedTask);
   }
   
   const handleSubmit = () => {
@@ -66,26 +73,83 @@ const TaskCreation = () => {
     >
       <section className={classes.modalContent}>
         <form className={classes.form}>
+          <label>
+          Name:
           <input 
             type="text"
             placeholder="Task Name"
             name="name"
             onChange={e => handleTaskChange(e)}
           />
+          </label>
+          <label>
+          Description:
           <input
             type="text"
             placeholder="Task Description"
             name="description"
             onChange={e => handleTaskChange(e)}
           />
+          </label>
           <label>
             Category:
           <select name="category" onChange={e => handleTaskChange(e)}>
-            <option name="category" value="Brain Training">Brain Training</option>
-            <option name="category" value="Health Training">Health Training</option>
-            <option name="category" value="Creativity Training">Creativity Training</option>
-            <option name="category" value="Basic Training">Basic Training</option>
+            <option value="Brain Training">Brain Training</option>
+            <option value="Health Training">Health Training</option>
+            <option value="Creativity Training">Creativity Training</option>
+            <option value="Basic Training">Basic Training</option>
           </select>
+          </label>
+          <label>
+            Point Value:
+            <input 
+              type="number"
+              name="points"
+              onChange={e => handleTaskChange(e)} 
+              min={1} 
+              max={20}
+            />
+          </label>
+          <label>
+            Photo Required
+            <label>
+              ❌
+              <input 
+                type="radio"
+                name="photo"
+                value={false}
+                onChange={(e) => handleTaskChange(e)}
+              />
+            </label>
+            <label>
+              ✅
+              <input
+                type="radio"
+                name="photo"
+                value={true}
+                onChange={(e) => handleTaskChange(e)}
+              />
+            </label>
+          </label>
+          <label>
+            Resource:
+            <select name="resource_type" onChange={e => handleTaskChange(e)}>
+              <option value="video">Youtube Video</option>
+              <option value="image">Image / Gif</option>
+              <option value="link">Link</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Description"
+              name="resource_alt"
+              onChange={e => handleTaskChange(e)}
+            />
+            <input
+              type="text"
+              placeholder="Link"
+              name="resource_link"
+              onChange={e => handleTaskChange(e)}
+            />
           </label>
         </form>
       </section>
