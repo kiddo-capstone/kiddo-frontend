1588271683f9edfc294b0fddce45f84801c7190e
Bailey Dunning <bwdunning@gmail.com>
2021-03-20 16:30:16 -0600

    create reward store

--- src/modules/views/RewardStore.jsx
+++ src/modules/views/RewardStore.jsx
@@ -1,5 +1,33 @@
-const RewardStore = () => {
+import { useContext, useEffect, useState } from "react";
+import { getRewardsByUserId, redeemReward } from "../common/apiCalls";
+import { PageContainer } from "../../ui/containers";
+import AppContext from "../App/AppContext";
+import RewardCard from "../rewards/RewardCard";
+
+const RewardStore = ({ id }) => {
+  const [rewards, setRewards] = useState([])
+  const [state, dispatch] = useContext(AppContext)
+
+  useEffect(() => {
+    getRewardsByUserId(id)
+      .then(data => setRewards(data.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  const makeRewardCards = () => {
+    if (rewards.length > 0) {
+      return rewards.map(reward => {
+        return <RewardCard key={reward.id} reward={reward}/>
+      })
+    }
+  }
+
   return (
-    <h1>Reward Store</h1>
+    <PageContainer>
+      <h1>Reward Store</h1>
+      { makeRewardCards() }
+    </PageContainer>
   )
 }
+
+export default RewardStore;
