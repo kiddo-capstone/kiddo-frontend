8b2a9b5daeab54c16d6da0d6fe52e8c25655aa51
Lola Dolinsky <dolinsky.lola@gmail.com>
2021-03-21 18:12:04 -0600

    Fix minor bug in ProgressBar

--- src/ui/dataVisuals/ProgressBar.js
+++ src/ui/dataVisuals/ProgressBar.js
@@ -32,11 +32,19 @@ const ProgressBar = (props) => {
       return <span style={label}>{`${completed}%`}</span>;
     } else if (total !== 100 && completed <= total) {
       return <span style={label}>{`${completed}/${total}`}</span>;
+    } else if (total === 100 && completed >= total) {
+      return (
+        <span style={label}>{`WOW! ${completed} points. Check out the`}</span>
+      );
     }
   };
   return (
     <div style={container}>
-      <div style={filler}>{determineTotalBar(total, completed)}</div>
+      <div style={filler}>
+        {total === 100 && <span style={label}>{`${completed}%`}</span>}
+        {total !== 100 && <span style={label}>{`${completed}/${total}`}</span>}
+        {/* {determineTotalBar(total, completed)} */}
+      </div>
     </div>
   );
 };
